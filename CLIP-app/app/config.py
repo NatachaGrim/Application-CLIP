cf2e290aede8a2903e5ba25846c72c2bc504b086
Natacha Grim <129739541+NatachaGrim@users.noreply.github.com>
2024-09-19 11:52:03 +0200

    Update config.py

--- CLIP-app/app/config.py
+++ CLIP-app/app/config.py
@@ -3,37 +3,49 @@ import os
 class Config():
     DEBUG = os.environ.get("DEBUG")
 
-# Configuration globale
+# Maximum number of images in query results
 maxResults = 300
+
+# Maximum number of images to display on home page
 maxThumbnails = 30
+
+# Threshold above which a result is considered valid
 minProb = 0.6
 gap = 0.8
 
-# Récupérer le dossier ciblé depuis la variable d'environnement
+# Retrieve target folder from the "FLASK_ARG" variable
 try:
     use_case = os.environ["FLASK_ARG"]
 except KeyError:
-    print("FLASK_ARG env var is not defined!")
+    print("FLASK_ARG is not defined!")
     quit()
 
-# Dossiers de l'application
+# Folder containing embeddings
 torchFolder = "app/models/"
-img_folder = f"app/static/{use_case}/images"
-eval_folder = "app/static/_eval/"
-conf_mat_file = f"{eval_folder}{use_case}_confusion.txt"
 
+# Folder containing the images
+img_folder = f"app/static/{use_case}/images"
+
+# Folder containing confidence scores
+eval_folder = "app/static/_eval/"
+
+# Folder containing the confusion matrices
+# conf_mat_file = f"{eval_folder}{use_case}_confusion.txt"
+
+# Get the list of files in the torchFolder
 fichiers = os.listdir(torchFolder)
 
-#tensor_file = f"{torchFolder}/royere_gouache_petite_fine_tuned.pt"
-
-#tensor_file = next((os.path.join(torchFolder, fichier) for fichier in fichiers if fichier.endswith(f"_{use_case}_fine_tuned.pt")), None)
-
-#if tensor_file is None:
-#tensor_file = f"{torchFolder}/08_14_2024_fine_tuned_royere_gouache_ft.pt"
-
+# File containing project image embeddings
 tensor_file = next((os.path.join(torchFolder, fichier) for fichier in fichiers if fichier.endswith(f"_{use_case}.pt")), None)
 
+# File containing the list of image paths
 directory_file = f"app/static/{use_case}/{use_case}_list.txt"
+
+# File containing the project summary
 directory_summary = f"app/static/{use_case}/{use_case}_directory.txt"
+
+# File containing image URLs
 image_urls = f"app/static/{use_case}/{use_case}_urls.txt"
+
+# File containing labels and captions
 labels_file = f"app/static/{use_case}/ontology/{use_case}_ontology.csv"
