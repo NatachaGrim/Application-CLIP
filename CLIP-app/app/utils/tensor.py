3e60f692d4be7a7d447f52a2d4276d8339ceb5eb
Natacha Grim <129739541+NatachaGrim@users.noreply.github.com>
2024-09-19 14:41:59 +0200

    Update tensor.py

--- CLIP-app/app/utils/tensor.py
+++ CLIP-app/app/utils/tensor.py
@@ -1,8 +1,17 @@
 import os
 from flask import current_app as app
 
-# Function that writes a tensor to a file in binary mode
 def write_tensor(tensor, filename):
+    """
+    This function writes a tensor to a file in binary mode. The file is only created if it does not already exist.
+
+    Arguments:
+        :tensor: a PyTorch tensor
+        :filename: name of the file in which the tensor will be written
+
+    Output:
+        A file containing one or more tensors
+    """
     if os.path.exists(filename):
         app.logger.info(f"{filename} already exists!")
         return
