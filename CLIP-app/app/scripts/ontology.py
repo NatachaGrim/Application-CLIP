e933ce58dea4aa7444bd502edab3f8ba80482f40
Natacha Grim <129739541+NatachaGrim@users.noreply.github.com>
2024-09-19 14:31:29 +0200

    Update inner doc for ontology.py

--- CLIP-app/app/scripts/ontology.py
+++ CLIP-app/app/scripts/ontology.py
@@ -1,50 +1,58 @@
+"""
+This script standardises the labels and captions in a .txt file before converting it to .csv format
+
+Arguments:
+    :flask_arg: project folder where to find the input .txt ontology file
+
+Output:
+    .csv file containing labels and captions
+    This file is saved in the 'app/static/<flask_arg>/ontology/<flask_arg>_ontology.csv
+"""
+
 import csv
 import os
 import unidecode
 
 def normalize_text(text):
-    # Convertir en minuscules
+    # Convert to lowercase
     text = text.lower()
-    # Supprimer les accents
+    # Remove accents
     text = unidecode.unidecode(text)
-    # Remplacer les points-virgules par un espace
+    # Replace semicolons with a space
     text = text.replace(';', ' ')
     return text
 
 def txt_to_csv(flask_arg):
-    # Construire le chemin vers le fichier .txt
+    # Path to .txt input ontology file
     txt_file_path = f"app/static/{flask_arg}/ontology/{flask_arg}_ontology.txt"
-    # Construire le chemin vers le fichier .csv
+    # Path to .csv output ontology file
     csv_file_path = f"app/static/{flask_arg}/ontology/{flask_arg}_ontology.csv"
 
     try:
         with open(txt_file_path, 'r', encoding='utf-8') as txt_file:
             lines = txt_file.readlines()
         
-        # Ouvrir le fichier CSV pour écrire avec l'encodage UTF-8
+        # Open .csv file
         with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
             writer = csv.writer(csv_file, delimiter=';')
             
-            # Traiter chaque ligne du fichier .txt
+            # Extract label and caption from each line
             for line in lines:
-                # Extraire le terme et la définition
                 parts = line.split('": "')
                 if len(parts) == 2:
                     term = parts[0].strip('"')
                     definition = parts[1].strip().strip('"')
                     
-                    # Normaliser le terme et la définition
+                    # Normalising labels and captions
                     term = normalize_text(term)
                     definition = normalize_text(definition)
-                    
-                    # Écrire dans le fichier CSV
                     writer.writerow([term, definition])
         
         print(f"Le fichier CSV a été créé avec succès : {csv_file_path}")
     except Exception as e:
         print(f"Une erreur s'est produite : {e}")
 
-# Récupérer FLASK_ARG depuis les variables d'environnement
+# Retrieve FLASK_ARG variable
 flask_arg = os.environ.get('FLASK_ARG')
 
 if not flask_arg:
