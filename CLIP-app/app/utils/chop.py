62408f4fcc66518331b244076cd806ad3770cdac
Natacha Grim <129739541+NatachaGrim@users.noreply.github.com>
2024-09-19 14:39:08 +0200

    Update chop.py

--- CLIP-app/app/utils/chop.py
+++ CLIP-app/app/utils/chop.py
@@ -1,3 +1,11 @@
-# Function returning the first five characters of a string
 def chop(string):
+    """
+    This function returns script the first five characters of a given string
+
+    Arguments:
+        :string: a string to chop
+
+    Output:
+        The five first characters of the string
+    """
     return string[:5]
